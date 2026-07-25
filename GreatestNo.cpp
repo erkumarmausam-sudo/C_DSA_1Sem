3f8a347e34add59b721d4d2e880d867587003ff2
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-07-26 00:19:57 +0530

    Add files via upload

---
+++ GreatestNo.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{  int a = 3 , b = 4;
+int result = (a<b) ? b : a;
+cout << " the greatest number  "
+"is  "<< result;
+
+
+    return 0;
+}
