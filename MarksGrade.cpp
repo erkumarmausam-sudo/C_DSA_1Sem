e5c180a8e8815d958fc5335198167e48601b9591
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-07-27 19:37:57 +0530

    Initial commit

---
+++ MarksGrade.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter a"<<endl;
+  cin>>a;
+  if (a>=80 && a<=100){
+      cout<<"A+ grade";
+  }
+  else if (a>=80 && a<=89){
+      cout <<"A grade";
+  }
+  else if (a>=60 && a<=69){
+      cout<<"C grade";
+  }
+  else if (a>=70 && a<=79){
+      cout<<"B grade";}
+      else if (a<=50 && a<=59) 
+          
+      
+   cout<<"fail";
+
+    return 0;
+}
