e1a4e3b5f2de7b7417a8ecc664d9f59fbaa6a46d
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-07-27 19:43:11 +0530

    Initial commit

---
+++ oprator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{  int p = 7;
+int q = 5;
+
+cout << "p==q:" << (p==q) << endl;
+cout << "p!=q:" << (p!=q) << endl;
+cout << "p>q:" << (p>q) << endl;
+cout << "p<q:" << (p<q) << endl;
+cout << "p>=q:" << (p>=q) << endl;
+cout << "p<=q:" << (p<=q) << endl;
+
+
+    return 0;
+}
