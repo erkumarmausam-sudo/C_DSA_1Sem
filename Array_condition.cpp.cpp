6bd91fbecbf13ab53e8db20291cba98613dcd136
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-07-31 23:47:05 +0530

    Initial commit

---
+++ Array_condition.cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, small;
+int array[5]={1,2,3,4,5};
+
+for(n=0;n<5;n++){
+if(small>=array[n])
+small=array[n];
+    
+}
+cout<<small<<endl;
+
+    return 0;
+}
