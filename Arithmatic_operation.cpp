e144f8293c3b89e3aadbb1d8b0dab1eea7e6e334
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-07-31 23:46:43 +0530

    Initial commit

---
+++ Arithmatic_operation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a = 10;
+int b = 3;
+cout<<"addition (a+b);"<<(a+b)<<endl;
+cout<<"substraction (a+b)"<<(a-b)<<endl;
+cout <<"multiplication (a* b)"<<(a*b)<< endl;
+cout << "division (a/b)"<< (a/b)<< endl;
+cout <<"modulo(a%b):" <<(a%b) << std::endl;
+double x = 10.5;
+double y = 2.5;
+cout <<"floating-point division (x/y):" <<(x/y) <<(x/y)<< std::endl;
+
+    
+
+    return 0;
+}
